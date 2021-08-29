472925ba4d6543d80ba1b96d1bfdd4cfbf460987
DEONTAY JEFFERSON <djefferson46@gmail.com>
2021-08-29 09:12:57 -0700

    Added the view page for products

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Testing.Models;
+
+namespace Testing
+{
+    interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+    }
+}
